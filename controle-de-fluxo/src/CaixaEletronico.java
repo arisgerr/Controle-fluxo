fa54b44a751286326a52fa78edce4282175238c4
arisger <ariana.eger.ti@gmail.com>
2025-04-30 07:39:17 -0300

    CaixaEletronico.java

--- controle-de-fluxo/src/CaixaEletronico.java
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -1,10 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
-        double saldo = 52.0;
+        double saldo = 12.0;
         double valorSolicitado = 15.07;
 
         if(valorSolicitado < saldo)
         saldo = saldo - valorSolicitado;
+
+        else 
+           System.out.println("Saldo insuficiente");
 
         System.out.println(saldo);
         
